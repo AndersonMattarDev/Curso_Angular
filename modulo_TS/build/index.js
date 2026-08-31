17de4e5d6757a138ebc048d97dd51daeef0a8710
anderson <andmat7745@gmail.com>
2026-08-31 17:16:56 -0300

    aula ts congig

---
+++ modulo_TS/build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printaObjeto(pessoa) {
+    console.log(pessoa);
+}
+printaObjeto({
+    name: "Bruce Wayne",
+    vulgo: "Batman"
+});
+//# sourceMappingURL=index.js.map
